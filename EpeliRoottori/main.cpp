86a5d18739d663d4ea6c09944fe86ce26b44385b
lolobster <nissinena@elisanet.fi>
2015-09-25 13:04:18 +0300

    peruskirjastot linkattu ja ikkuna aukee

--- EpeliRoottori/main.cpp
+++ EpeliRoottori/main.cpp
@@ -1,8 +1,38 @@
+#define GLM_FORCE_RADIANS
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
 
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
 
+// GLFW
+#include <GLFW/glfw3.h>
+
+// GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform.hpp>
+
+// Ikkunan koko
+const GLuint WIDTH = 1200, HEIGHT = 800;
 
 int main(void)
 {
+	glfwInit();			// Initialisoidaan GLFW
+
+	// GLFW perusasetukset
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);			// asetetaan OpenGL-
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);			// versio 2.1
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_ANY_PROFILE);	// m‰‰ritell‰‰n OpenGL profiili k‰ytett‰v‰ksi
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);				// ikkunan kokoa ei voi muuttaa
+
+
+	// luodaan ikkuna GLFWwindow
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "EPELIROOTTORI", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+
 	// :DDDDDDDDDDD
 	// haistappa eemeli :DDD
 	// pasha :DDDDDDDDDDDDD
