81f2f0a8ea5a88541f05916726bff4ee39895d2f
latexd <lauri.hankilanoja@hotmail.com>
2015-10-14 10:53:06 +0300

    Tekstimanagerin alkua, matikkapuoli vielä vähän hakusessa

--- EpeliRoottori/TextManager.cpp
+++ EpeliRoottori/TextManager.cpp
@@ -3,9 +3,67 @@
 
 TextManager::TextManager()
 {
-}
+	// Defaults to an empty message with white color and charactersize of 12
 
+	text = "";
+	color.resize(4,255);
+	characterSize = 12.0f;
+}
 
 TextManager::~TextManager()
 {
+
+}
+
+void TextManager::LoadFont(std::string filepath)
+{
+	std::ifstream file;
+	file.open(filepath);
+	if (file.is_open())
+	{
+		while (!file.eof())
+		{
+
+		}
+	}
+	else
+	{
+		std::cout << "The font file was not found." << std::endl;
+	}
+}
+
+void TextManager::SetCharacterSize(float size)
+{
+	characterSize = size;
+}
+
+void TextManager::SetColor(int red, int blue, int green, int opacity)
+{
+	// Varmaan kirjoittelen vielä värille oman luokan
+	// Sen avulla voi sitten muokata yhtä väriarvoa kerrallaan, yms.
+
+	color[0] = red;
+	color[1] = blue;
+	color[2] = green;
+	color[4] = opacity;
+}
+
+void TextManager::SetText(std::string message)
+{
+	text = message;
+}
+
+float TextManager::GetCharacterSize()
+{
+	return characterSize;
+}
+
+std::vector<int> TextManager::GetColor()
+{
+	return color;
+}
+
+std::string TextManager::GetText()
+{
+	return text;
 }
--- EpeliRoottori/TextManager.h
+++ EpeliRoottori/TextManager.h
@@ -1,11 +1,28 @@
 #ifndef TEXTMANAGER_H
 #define TEXTMANAGER_H
 
+#include <iostream>
+#include <fstream>
+#include <vector>
+
 class TextManager
 {
 public:
 	TextManager();
 	~TextManager();
+
+	void LoadFont(std::string filepath);
+	void SetCharacterSize(float size);
+	void SetColor(int red, int blue, int green, int opacity);
+	void SetText(std::string message);
+	float GetCharacterSize();
+	std::vector<int> GetColor();
+	std::string GetText();
+
+private:
+	std::string text = "";
+	std::vector<int> color;
+	float characterSize = 12.0f;
 };
 
 #endif
