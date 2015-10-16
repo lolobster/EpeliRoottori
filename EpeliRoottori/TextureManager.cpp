dca8ba4a32d8328e9a438241c8b4f11fa6715299
lolobster <jallumies1>
2015-10-16 10:19:39 +0300

    vanhat koodit pois TextureManagerista

--- EpeliRoottori/TextureManager.cpp
+++ EpeliRoottori/TextureManager.cpp
@@ -27,18 +27,6 @@ TextureManager::~TextureManager()
 // ladataan PNG ensin muistiin ja dekoodataan siellä pixeleiksi
 void TextureManager::loadTextures(const char* filename)
 {
-/// shaderiin flippi tekstuureille
-
-	/*
-
-	image = SOIL_load_image("Dickbutt.png", width, height, 0, SOIL_LOAD_RGB);
-	
-
-	if (*image == NULL) // lataus ei onnistu --> virheilmotus
-	{
-		std::cout << "ei vittu lataa: " << SOIL_last_result() << std::endl;
-	}
-	*/
 	std::vector<unsigned char> png;
 	std::vector<unsigned char> texture;		
 	unsigned width, height;
