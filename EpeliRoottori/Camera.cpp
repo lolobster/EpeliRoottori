459da29ee483c71f11bc3ef49a566dfa66d022fd
lolobster <nissinena@elisanet.fi>
2015-10-30 12:01:46 +0200

    Cameraan kivaa

--- EpeliRoottori/Camera.cpp
+++ EpeliRoottori/Camera.cpp
@@ -1,8 +1,14 @@
 #include "Camera.h"
 
 
-Camera::Camera()
+Camera::Camera(vec3 camPos, float camRotate, float camScale)
 {
+	camPos.x = 100;
+	camPos.y = 100;
+	camPos.z = 0;
+
+	camRotate = 0;
+	camScale = 1;
 }
 
 
@@ -10,12 +16,22 @@ Camera::~Camera()
 {
 }
 
-void Camera::update()
+void Camera::setPosition(vec3 newPos, vec3 camPos)
+{
+	camPos = newPos;
+}
+
+const vec3& Camera::getPosition()
+{
+	return *camPos;
+}
+
+void Camera::setScale(float camScale)
 {
 
 }
 
-void Camera::setPosition(float x, float y, float z)
+void Camera::setRotation(float camRotate)
 {
 
 }
