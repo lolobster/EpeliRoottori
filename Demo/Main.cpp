bc8b348cb56a3ae88f732dbfa29ae963f172db28
lolobster <nissinena@elisanet.fi>
2015-12-08 12:01:58 +0200

    MathClass poistettu, engine linkattu Demoon

--- Demo/Main.cpp
+++ Demo/Main.cpp
@@ -1,4 +1,11 @@
- 
+#include "EpeliRoottori/AnimationManager.h"
+#include "EpeliRoottori/Camera.h"
+#include "EpeliRoottori/Polygon.h"
+#include "EpeliRoottori/Renderer.h"
+#include "EpeliRoottori/Shader.h"
+#include "EpeliRoottori/TextManager.h"
+#include "EpeliRoottori/TextureManager.h"
+
 
 
 int main()
--- EpeliRoottori/MathClass.cpp
+++
@@ -1,9 +0,0 @@
-#include "MathClass.h"
-
-MathClass::MathClass()
-{
-}
-
-MathClass::~MathClass()
-{
-}
