28b774559f8c8847d2d1f5f153736f7e43198f67
lolobster <nissinena@elisanet.fi>
2015-12-13 20:25:47 +0200

    Timer pohja animaatioita varten

---
+++ EpeliRoottori/Timer.cpp
@@ -0,0 +1,91 @@
+#include "Timer.h"
+
+
+
+Timer::Timer()
+{
+	startTime = 0.0f;
+	stopTime = 0.0f;
+	pauseTime = 0.0f;
+	localTime = 0.0f;
+
+	started = false;
+	paused = false;
+}
+
+GLfloat Timer::getGlobalTime()
+{
+	return static_cast<GLfloat>(glfwGetTime());
+}
+
+GLfloat Timer::getLocalTime()
+{
+	localTime = 0.0f;
+
+	if (started)
+	{
+		if (paused)
+		{
+			localTime = pauseTime;
+		}
+		else
+		{
+			localTime = getGlobalTime() - startTime;
+		}
+	}
+	return localTime;
+}
+
+GLfloat Timer::start()
+{
+	started = true;
+	paused = false;
+
+	startTime = getGlobalTime();
+	pauseTime = 0.0f;
+	return startTime;
+}
+
+GLfloat Timer::stop()
+{
+	started = false;
+	paused = false;
+
+	stopTime = getGlobalTime();
+
+	startTime = 0.0f;
+	pauseTime = 0.0f;
+	return stopTime;
+}
+
+GLfloat Timer::pause()
+{
+	if (started && paused) // Unpause.
+	{
+		paused = false;
+		startTime = getGlobalTime() - pauseTime;
+		pauseTime = 0.0f;
+		return startTime;
+	}
+	else if (started == true && paused == false) // Pause
+	{
+		paused = true;
+
+		pauseTime = getGlobalTime() - startTime;
+		return pauseTime;
+	}
+	else
+	{
+		return pauseTime;
+	}
+}
+
+bool Timer::isStarted()
+{
+	return started;
+}
+
+bool Timer::isPaused()
+{
+	return paused && started;
+}
---
+++ EpeliRoottori/Timer.h
@@ -0,0 +1,47 @@
+#pragma once
+#ifndef TIMER_H
+#define TIMER_H
+
+#include "GLFW\glfw3.h"
+#include "GL\glew.h"
+
+
+class Timer
+{
+public:
+	Timer();
+	~Timer(){};
+
+	// Returns the Global time in ms.
+	static GLfloat getGlobalTime();
+
+	// Returns the local time in ms.
+	GLfloat getLocalTime();
+
+	// Start the timer.
+	GLfloat start();
+
+	// Pause the timer and continue from pause.
+	GLfloat pause();
+
+	// Stop the timer.
+	GLfloat stop();
+
+	// Timer update.
+	void update();
+
+	// Returns whether the timer is started or not.
+	bool isStarted();
+
+	// Returns whether the timer is paused or not.
+	bool isPaused();
+private:
+	GLfloat startTime;
+	GLfloat stopTime;
+	GLfloat pauseTime;
+	GLfloat localTime;
+	bool started;
+	bool paused;
+};
+
+#endif
